09bebfe669500df556e5c7362921c90c3f4192cf
Nick Van Wiggeren <nvanwiggeren@digitalocean.com>
2014-12-13 15:22:53 -0500

    wasted: assign var properly

--- cmd/wasted.go
+++ cmd/wasted.go
@@ -16,40 +16,40 @@ const wastedurl = "http://reddit.com/r/wastedgifs.json"
 
 func Wasted(msg *Message) {
 	data, err := getSite(wastedurl)
-	
+
 	if err != nil {
 		msg.Return("Error getting reddit data!")
 		return
 	}
-	
+
 	var rp RedditPage
 	json.Unmarshal(data, &rp)
-	
+
 	rand.Seed(time.Now().UTC().UnixNano())
 	randIndex := rand.Intn(len(rp.Data.Children))
-	
+
 	wasteddata := rp.Data.Children[randIndex].ChildData
 	var out string
-	
-	wasteddata.Url := fixLink(wasteddata.Url)
-	
+
+	wasteddata.Url = fixLink(wasteddata.Url)
+
 	if wasteddata.Nsfw {
 		out = fmt.Sprintf("(NSFW) %s: %s", wasteddata.Title, wasteddata.Url)
 	} else {
 		out = fmt.Sprintf("%s: %s", wasteddata.Title, wasteddata.Url)
 	}
-	
+
 	msg.Return(out)
 }
 
 // Converts a link to a direct link if it's on imgur
 func fixLink(link string) string {
 	originalurl, err := url.Parse(link)
-	
+
 	if err != nil || originalurl.Host != "imgur.com" {
 		return link
 	}
-	
+
 	return fmt.Sprintf("http://i.imgur.com%s.gif", originalurl.Path)
 }
 
@@ -58,8 +58,8 @@ type RedditPage struct {
 		Children []struct {
 			ChildData struct {
 				Title string `json:"title"`
-				Nsfw bool    `json:"over_18"`
-				Url string   `json:"url"`
+				Nsfw  bool   `json:"over_18"`
+				Url   string `json:"url"`
 			} `json:"data"`
 		} `json:"children"`
 	} `json:"data"`
