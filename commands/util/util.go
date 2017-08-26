4e1093ea5cd4df5cca206e1177f8441911f8918f
Andrey Radchishin <andreyrd1@gmail.com>
2017-08-26 15:37:12 -0700

    utils: Fetch returns error on non 2xx status code

--- commands/util/util.go
+++ commands/util/util.go
@@ -18,6 +18,9 @@ func Fetch(url string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("Non 2xx status code returned: %d", resp.StatusCode)
+	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
