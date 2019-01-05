bafab7f6a388afef13b5e03b5829a28fc227baed
Nicholas Van Wiggeren <nick@facepwn.com>
2019-01-04 17:08:58 -0800

    use better errors

--- commands/util/geolocate.go
+++ commands/util/geolocate.go
@@ -25,7 +25,7 @@ func GetCoordinates(addr []string) (*GoogleReturn, error) {
 	geoURL := fmt.Sprintf("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s&sensor=false", address, apiKey)
 	data, err := Fetch(geoURL)
 	if err != nil {
-		return nil, ErrInvalidAddress
+		return nil, errors.New("error geolocating your coordinates from Google")
 	}
 	var geo geoLocation
 	json.Unmarshal(data, &geo)
